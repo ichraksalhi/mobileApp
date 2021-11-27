f108265355415a38487fdcd6f900a32ba3413cd6
ichraks@yahoo.com <ichraks@yahoo.com>
2021-11-27 19:28:01 +0100

    display results in consol

--- App.js
+++ App.js
@@ -1,21 +1,57 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React , {useState} from 'react';
+import { StyleSheet, Text, View , TextInput , ScrollView} from 'react-native';
+import axios from 'axios';
 
 export default function App() {
+  const apiurl = "http://www.omdbapi.com/?i=tt3896198&apikey=ca5bdf82";
+  const [state , setState] = useState({
+    s:"",
+    results : [], // where i will put req
+    selected: {}
+
+  });
+
+  const search = () => {
+    axios (apiurl + "&s=" + state.s ).then(({ data })=> {
+      let results = data.Search
+      console.log('results',results)
+      setState(prevState => {
+        return {...prevState,results:results}
+      })
+    })
+  }
+
+
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>FIND YOUR MOVIE</Text>
+      <TextInput 
+      style={styles.searchbox}
+      
+      onChangeText={text => setState(prevState => {
+        return {...prevState, s: text}
+      }) }
+      onSubmitEditing={search}
+      value={state.s}
+      />
     </View>
   );
 }
 
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#D51F51',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  searchbox : {
+   fontSize : 20,
+   backgroundColor : '#fff' 
+  
+  }
+,
 });
